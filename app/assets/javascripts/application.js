40d80ff5d6f0966afe38da891a1048046a9e4d72
glz <837609221@qq.com>
2016-03-25 11:33:06 +0800

    Add NProgress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,9 @@
 //= require bootstrap-sprockets
 //= require simditor
 //= require_tree .
+
+
+// nprogress
+$(document).on('page:fetch', function() { NProgress.start(); });
+$(document).on('page:change', function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
