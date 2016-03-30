f01106ebbe8e501c114a6b9c09140f2422be143a
glz <837609221@qq.com>
2016-03-30 18:51:59 +0800

    Use deploy production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
+server '115.28.137.197', user: 'glz', roles: %w(web app db), my_property: :my_value
 
 # role-based syntax
 # ==================
