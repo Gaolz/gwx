322af630ccfb16cb151a28af2acd144837fa1138
glz <glzztt@gmail.com>
2016-09-25 16:37:06 +0800

    Add a new reciever for birthday

--- app/jobs/birthday_message_job.rb
+++ app/jobs/birthday_message_job.rb
@@ -5,7 +5,10 @@ class BirthdayMessageJob < ApplicationJob
 
   def perform(user_id)
     user = User.find(user_id)
-    glz = User.first
-    ChinaSMS.to '13730820749', "【高老庄】亲爱的#{glz.name}，今天是你的亲人#{user.name}，#{user.age}岁的生日，请对#{user.zh_sex}说声“生日快乐”，方便的话，也不要吝啬的你的双手，给#{user.zh_sex}一个真诚的拥抱。"
+    mobiles = %w(13730820749 18382012700)
+    recievers = User.where(mobile: mobiles)
+    recievers.each do |reciever|
+      ChinaSMS.to reciver.mobile, "【高老庄】亲爱的#{reciever.name}，今天是你的亲人#{user.name}，#{user.age}岁的生日，请对#{user.zh_sex}说声“生日快乐”，方便的话，也不要吝啬的你的双手，给#{user.zh_sex}一个真诚的拥抱。"
+    end
   end
 end
