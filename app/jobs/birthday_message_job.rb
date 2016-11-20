44bef945848c1f8920af8db36e85b867847635d5
glz <glzztt@gmail.com>
2016-11-20 23:18:11 +0800

    Fix birthday message job

--- app/jobs/birthday_message_job.rb
+++ app/jobs/birthday_message_job.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class BirthdayMessageJob < ApplicationJob
   self.queue_adapter = :sucker_punch
   include SuckerPunch::Job
@@ -6,7 +8,7 @@ class BirthdayMessageJob < ApplicationJob
   def perform(user_id)
     user = User.find(user_id)
     mobiles = %w(13730820749 18382012700)
-    receivers = User.where(mobile: mobile)
+    receivers = User.where(mobile: mobiles)
     receivers.each do |receiver|
       ChinaSMS.to receiver.mobile, "【高老庄】亲爱的#{reciever.name}，今天是你的亲人#{user.name}，#{user.age}岁的生日，请对#{user.zh_sex}说声“生日快乐”，方便的话，也不要吝啬的你的双手，给#{user.zh_sex}一个真诚的拥抱。"
     end
